cf0533632e7b6c7a169b6273e51b262a24b1e505
TomIA24 <169178002+TomIA24@users.noreply.github.com>
2024-07-25 15:24:18 +0200

    migration

---
+++ models/quizz.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Joi = require('joi');
+const { questionSchema, validateQuestion } = require('./question');
+
+const quizSchema = new mongoose.Schema({
+    questions: [questionSchema],
+    quizName: { type: String, required: true },
+    score: { type: Number, required: true },
+    incorrectlyAnsweredQuestions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+    flaggedQuestions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }]
+});
+
+const Quiz = mongoose.model("Quiz", quizSchema);
+
+const validateQuiz = (data) => {
+    const schema = Joi.object({
+        questions: Joi.array().items(validateQuestion).required().label("questions"),
+        quizName: Joi.string().required().label("quizName"),
+        score: Joi.number().required().label("score"),
+        incorrectlyAnsweredQuestions: Joi.array().items(Joi.string()).label("incorrectlyAnsweredQuestions"),
+        flaggedQuestions: Joi.array().items(Joi.string()).label("flaggedQuestions")
+    });
+    return schema.validate(data);
+};
+
+module.exports = { Quiz, validateQuiz };
