1898f1d9dfc73eb79ba33cc0cfa91673473a341a
hamdizer <zeramdinihamdi1@gmail.com>
2024-08-13 21:10:07 +0100

    paginated_list_courses

--- routes/courseControle.js
+++ routes/courseControle.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const router = require("express").Router();
 const { Course } = require("../models/course");
 const jwt = require("jsonwebtoken");
-const ObjectId = require('mongodb').ObjectID;
+const ObjectId = require("mongodb").ObjectID;
 const { Admin } = require("../models/Admin");
 const { Category } = require("../models/Category");
 
@@ -21,12 +21,21 @@ function authenticateToken(req, res, next) {
     next();
   });
 }
-
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const courses = await Course.find();
-    console.log("test");
-    res.status(200).send({ data: courses, message: "All courses" });
+    const page = parseInt(req.query.page) || 1; // Current page number
+    const limit = parseInt(req.query.limit) || 10; // Number of items per page
+    const skip = (page - 1) * limit; // Number of items to skip
+    const totalItems = await Course.countDocuments();
+
+    const courses = await Course.find({}).skip(skip).limit(limit);
+
+    res.status(200).send({
+      data: courses,
+      currentPage: page,
+      totalPages: Math.ceil(totalItems / limit),
+      totalItems,
+    });
   } catch (error) {
     //console.log(error)
     res.status(500).send({ message: "Internal Server Error", error: error });
@@ -71,124 +80,118 @@ router.post("/CreateCourse", authenticateToken, async (req, res) => {
 });
 
 router.get("/specific", async (req, res) => {
-    try {
-        const id = req.query.id;
-        console.log(id);
-        const course = await Course.findById(ObjectId(id));
-        console.log(course);
-        res.status(200).send({ data: course, message: "One course" });
-    } catch (error) {
-        //console.log(error)
-        res.status(500).send({ message: "Internal Server Error", error: error });
-        //console.log(error)
-    }
+  try {
+    const id = req.query.id;
+    console.log(id);
+    const course = await Course.findById(ObjectId(id));
+    console.log(course);
+    res.status(200).send({ data: course, message: "One course" });
+  } catch (error) {
+    //console.log(error)
+    res.status(500).send({ message: "Internal Server Error", error: error });
+    //console.log(error)
+  }
 });
 
 //
 router.post("/specificGroupe", authenticateToken, async (req, res) => {
-    try {
-        console.log(req.body);
+  try {
+    console.log(req.body);
 
-        const course = await Course.find({ _id: { $in: req.body.cardIds } });
-        // console.log(course)
-        res.status(200).send({ data: course, message: "All Course" });
-    } catch (error) {
-        console.log("error : ", error);
-        res.status(500).send({ message: "Internal Server Error", error: error });
-        //console.log(error)
-    }
+    const course = await Course.find({ _id: { $in: req.body.cardIds } });
+    // console.log(course)
+    res.status(200).send({ data: course, message: "All Course" });
+  } catch (error) {
+    console.log("error : ", error);
+    res.status(500).send({ message: "Internal Server Error", error: error });
+    //console.log(error)
+  }
 });
 router.post("/specificGroupeFromCategory", async (req, res) => {
-    try {
-        console.log(req.body);
-        console.log("here: ", req.body);
-        if (Array.isArray(req.body)) {
-            const course = await Course.find({ _id: { $in: req.body } });
-            // console.log(course)
-            return res.status(200).send({ data: course, message: "All Course" });
-        }
-        const course = await Course.find();
-        res.status(200).send({ data: course, message: "All Course" });
-
-    } catch (error) {
-        console.log("error : ", error);
-        res.status(500).send({ message: "Internal Server Error", error: error });
-        //console.log(error)
+  try {
+    console.log(req.body);
+    console.log("here: ", req.body);
+    if (Array.isArray(req.body)) {
+      const course = await Course.find({ _id: { $in: req.body } });
+      // console.log(course)
+      return res.status(200).send({ data: course, message: "All Course" });
     }
+    const course = await Course.find();
+    res.status(200).send({ data: course, message: "All Course" });
+  } catch (error) {
+    console.log("error : ", error);
+    res.status(500).send({ message: "Internal Server Error", error: error });
+    //console.log(error)
+  }
 });
 
 router.post("/deleteCourse", authenticateToken, async (req, res) => {
+  try {
+    const id = req.user["_id"];
+    //console.log(req.body.idCourse)
+    const admin = await Admin.findOne({ _id: ObjectId(id) });
+    //console.log(admin)
 
-    try {
-        const id = req.user["_id"]
-        //console.log(req.body.idCourse)
-        const admin = await Admin.findOne({ _id: ObjectId(id) });
-        //console.log(admin)
-
-        if (admin) {
-
-            await Course.deleteOne({ _id: ObjectId(req.body.idCourse) });
-            res.status(201).send({ message: "user deleted successfully" });
-            //console.log(users)
-        }
-        //res.status(401).send({ message: "you are not allowed to access to this data" });
-
-    } catch (error) {
-        res.status(500).send({ message: "Internal Server Error", error: error });
-        console.log("/////////", error)
+    if (admin) {
+      await Course.deleteOne({ _id: ObjectId(req.body.idCourse) });
+      res.status(201).send({ message: "user deleted successfully" });
+      //console.log(users)
     }
+    //res.status(401).send({ message: "you are not allowed to access to this data" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error", error: error });
+    console.log("/////////", error);
+  }
 });
 
 router.post("/updateCourse", authenticateToken, async (req, res) => {
+  const id = req.user["_id"];
+  //console.log(req.body)
+  try {
+    // const { error } = validate(req.body);
+    // if (error)
+    //     return res.status(400).send({ message: error.details[0].message });
+    await Course.updateOne({ _id: req.body._id }, { $set: req.body });
 
-    const id = req.user["_id"]
-    //console.log(req.body)
-    try {
-        // const { error } = validate(req.body);
-        // if (error)
-        //     return res.status(400).send({ message: error.details[0].message });
-        await Course.updateOne({ _id: req.body._id }, { $set: req.body })
-
-        res.status(201).send({ message: "Course updated successfully" });
-    } catch (error) {
-        res.status(500).send({ message: "Internal Server Error", error: error });
-        console.log("/////////", error)
-    }
+    res.status(201).send({ message: "Course updated successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error", error: error });
+    console.log("/////////", error);
+  }
 });
 
-
-
-
 function sum(array) {
-    let sum = 0;
-    console.log(array)
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i].rate;
-    }
+  let sum = 0;
+  console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i].rate;
+  }
 
-    return sum
+  return sum;
 }
 
 router.post("/Evaluate", authenticateToken, async (req, res) => {
-    const id = req.user["_id"]
-    try {
-        console.log("//////////", req.body.courseId)
-        const course = await Course.findOne({ "_id": ObjectId(req.body.courseId) })
-        console.log(course)
-        var sommeRating = sum(course.evaluate)
+  const id = req.user["_id"];
+  try {
+    console.log("//////////", req.body.courseId);
+    const course = await Course.findOne({ _id: ObjectId(req.body.courseId) });
+    console.log(course);
+    var sommeRating = sum(course.evaluate);
 
-        var rating = (sommeRating + req.body.Data.rate) / (course.evaluate.length + 1)
+    var rating =
+      (sommeRating + req.body.Data.rate) / (course.evaluate.length + 1);
 
+    await Course.updateOne(
+      { _id: req.body.courseId },
+      { $push: { evaluate: req.body.Data }, $set: { rating: rating } }
+    );
 
-        await Course.updateOne({ _id: req.body.courseId }, { $push: { evaluate: req.body.Data }, $set: { rating: rating } })
-
-        res.status(200).send({ message: "Evaluation done" })
-        console.log("done")
-    } catch (error) {
-        res.status(500).send({ message: "Internal Server Error", error: error });
-        console.log("/////////", error)
-    }
-})
-
+    res.status(200).send({ message: "Evaluation done" });
+    console.log("done");
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error", error: error });
+    console.log("/////////", error);
+  }
+});
 
 module.exports = router;
